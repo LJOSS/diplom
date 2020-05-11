91a1ffafb051367c3c42274d60f84b9c8f8132ba
Иван Китаев <ivanbestboy@gmail.com>
2020-05-11 16:08:24 +0300

    init Project

---
+++ app/src/main/java/com/example/my_child/App.kt
@@ -0,0 +1,10 @@
+package com.example.my_child
+
+import androidx.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
