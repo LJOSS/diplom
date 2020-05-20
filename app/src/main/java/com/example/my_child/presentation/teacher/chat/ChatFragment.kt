ff4809e08b3957c66348fda0e4b68cb5facb4025
Иван Китаев <ivanbestboy@gmail.com>
2020-05-20 11:34:41 +0300

    добавил layout для чата

--- app/src/main/java/com/example/my_child/presentation/teacher/chat/ChatFragment.kt
+++ app/src/main/java/com/example/my_child/presentation/teacher/chat/ChatFragment.kt
@@ -1,6 +1,25 @@
 package com.example.my_child.presentation.teacher.chat
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.my_child.R
 import com.example.my_child.presentation.fragments.BaseFragment
 
-class ChatFragment:BaseFragment() {
+class ChatFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.chat_layout, null, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val viewModel = ViewModelProvider(this, ChatViewModelFactory())
+            .get(ChatViewModel::class.java)
+    }
 }
